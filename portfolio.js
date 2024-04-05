46e2f888fd852d099a5657fb7d13a941ce8a081a
Sekoucisse.ehb <sekou.cisse@student.ehb.be>
2024-04-05 23:00:15 +0200

    changement

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+'user strict'
+const mijnhoofttittle = document.getElementById("mijn-hooft-tittle");
+mijnhoofttittle.style.backgroundColor = " rgb(57, 135, 135)";
+mijnhoofttittle.style.textAlign = " center";
+mijnhoofttittle.style.fontFamily = "Georgia, Times, 'Times New Roman', serif"
+mijnhoofttittle.innerText = " Mijn Portfolio van Web Advenced "
+
