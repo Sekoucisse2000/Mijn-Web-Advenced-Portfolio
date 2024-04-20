6a56e249cc1be9e49091e67dd90396227d9c59f6
Sekoucisse.ehb <sekou.cisse@student.ehb.be>
2024-04-21 01:15:17 +0200

    changments

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,15 @@
-/*user strict'
+'user strict'
 const mijnhoofttittle = document.getElementById("mijn-hooft-tittle");
 mijnhoofttittle.style.backgroundColor = " rgb(57, 135, 135)";
 mijnhoofttittle.style.textAlign = " center";
 mijnhoofttittle.style.fontFamily = "Georgia, Times, 'Times New Roman', serif"
 mijnhoofttittle.innerText = " Mijn Portfolio van Web Advenced "
-*/
-'use strict'
+
+
 const mijnvraag = document.querySelector(".klik-event");
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const antwoord = document.querySelector("p");
-
-
-
-
-
-
-
-
 
 mijnvraag.addEventListener("click", () => {
 
